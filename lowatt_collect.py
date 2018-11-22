59a599d196ddc2e2393e60e3ed001a96414d0878
Sylvain Thénault <sylvain@sytweb.org>
2018-11-22 11:00:22 +0100

    Move doc from module to README

--- lowatt_collect.py
+++ lowatt_collect.py
@@ -5,88 +5,7 @@
 **Command line interface and misc tools to collect distant data and do something
 about it**
 
-This is driven by a 'sources' definition YAML_ file. Each source may either
-define sub-sources or have a 'collect' value indicating the shell command to use
-to collect data and/or a 'postcollect' value indicating the shell command to
-start when a new file is collected.
-
-One source may only have 'postcollect' defined without any 'collect' in case
-where files are put in there by hand.
-
-Last but not least, each collect is done within a temporary directory. Collected
-files are moved from this directory to the sources hierarchy once fully
-collected (e.g. downloaded) and all postcollect treatments occured (e.g. data is
-imported in a database). If some error occurs during postcollect, it's moved in
-the 'errors' subdirectory of the source directory.
-
-Below a sample source file:
-
-.. code-block:: yaml
-
-    root: /data/
-    environment:
-      CONFIG_DIR: /conf
-
-    sources:
-
-      meteofrance:
-        collect: "python -m des.mf -o $DIR"
-        postcollect: "python -m des.dataimport des.mf.read_csv_stream $FILE"
-
-      tsme:
-
-        bill:
-          collect: "python -m des.suez dl-bill -I $ROOT/index_tsme.json -o $DIR $CONFIG_DIR//tsme.yml"
-          postcollect: "python -m des.dataimport des.suez.read_pdf_stream $FILE"
-
-        index:
-          collect: "python -m des.suez dl-index -o $DIR $CONFIG_DIR//tsme.yml"
-          postcollect: "python -m des.dataimport des.suez.read_xls_stream $FILE"
-
-      des-conseil:
-        collect:
-        postcollect: "python -m des.dataimport des.scripts.read_xls_stream $FILE"
-
-
-Sources hierarchy will be mirrored under the directory specified as 'root' value. The
-above example would be mapped to:
-
-.. code-block:: text
-
-  /data
-    /des-conseil
-      <files put there by hand and importable by des.suez.read_xls_stream>
-    /meteofrance
-      <files collected from meteofrance and importable by des.mf.read_csv_stream>
-    /tsme
-      /bill
-        <files collected from tsme and importable by des.suez.read_pdf_stream>
-      /index
-        <files collected from tsme and importable by des.suez.read_xls_stream>
-
-
-Each 'collect' and 'postcollect' shell command may use environment
-variables. Environment variables available are:
-
-* those inherited from the process that launched the collect
-
-* those defined in the 'environment' section of the configuration file
-
-* 'SOURCE': root source key from which the command is coming
-
-* 'COLLECTOR': path from root to the collector joined by '.' - same as 'SOURCE'
-  if the collector is defined at the first level in the hierarchy
-
-* 'ROOT': path to the root directory
-
-* 'FILE': path to the file to treat (**postcollect only**)
-
-* 'DIR': source directory - this may not be the actual directory under 'ROOT'
-  but a temporary directory, as collect happen within a temporary directory
-  whose content is moved once collect and postcollect are done
-
-* 'LOG_LEVEL' = the log level name received as argument ('DEBUG', 'INFO',
-  'WARNING' or 'ERROR')
+See the README file for documentation on usage and configuration.
 
 .. autofunction:: collect
 .. autofunction:: postcollect
@@ -94,7 +13,7 @@ variables. Environment variables available are:
 .. autofunction:: postcollect_commands
 .. autofunction:: files_postcollect_commands
 .. autofunction:: source_defs
-"""  # noqa
+"""
 
 from abc import ABC, abstractmethod
 from concurrent import futures
