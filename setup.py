792b09289df4afae028fe8ccf985fd874d20454b
Sylvain Thénault <sylvain@sytweb.org>
2018-12-11 16:16:16 +0100

    Fix console script entry point binding

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'lowatt-collect=lowat_collect:run',
+            'lowatt-collect=lowatt_collect:run',
         ],
     },
 )
