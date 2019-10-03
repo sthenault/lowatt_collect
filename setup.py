be061754b1240fe04352ab3221f9ca58aa663ada
Sylvain Thénault <sylvain@lowatt.fr>
2019-10-03 11:43:43 +0200

    Publish version 1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='lowatt_collect',
-    version='1.0',
+    version='1.1',
     url='https://github.com/lowatt/lowatt_collect',
 
     license='GPL3',
