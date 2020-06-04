ddf8f8a872703d49a952cf57baef01ea25e28fa2
Sylvain Thénault <sylvain@lowatt.fr>
2020-06-04 09:56:45 +0200

    [test] Use consistent multi-lines statement wrapping

--- test/test_lowatt_collect.py
+++ test/test_lowatt_collect.py
@@ -93,31 +93,47 @@ class CollectTC(unittest.TestCase):
                         },
                     },
                     env={'TEST': 'test', 'HERE': dirname(__file__)},
-                    root_directory=tmpdir)
+                    root_directory=tmpdir,
+                )
 
             self.assertEqual(
                 sorted(msg.split(' No such file')[0] for msg in cm.output),
-                ['ERROR:lowatt.collect:Error running crashmeforsure: '
-                 "[Errno 2]",
-                 "ERROR:lowatt.collect:Error running crashmeforsure: "
-                 "[Errno 2]"])
+                [
+                    'ERROR:lowatt.collect:Error running crashmeforsure: '
+                    "[Errno 2]",
+                    "ERROR:lowatt.collect:Error running crashmeforsure: "
+                    "[Errno 2]",
+                ],
+            )
 
             self.assertEqual(len(errors), 2)
 
-            self.assertEqual(listdir(tmpdir),
-                             ['s1', 's2'])
-            self.assertEqual(listdir(join(tmpdir, 's1')),
-                             ['.index.json', 'ack', 'errors'])
-            self.assertEqual(listdir(join(tmpdir, 's1', 'errors')),
-                             ['s1.file'])
-            self.assertEqual(listdir(join(tmpdir, 's2')),
-                             ['sub1', 'sub2'])
-            self.assertEqual(listdir(join(tmpdir, 's2', 'sub1')),
-                             ['.index.json', 'ack', 'sub1.file'])
+            self.assertEqual(
+                listdir(tmpdir),
+                ['s1', 's2'],
+            )
+            self.assertEqual(
+                listdir(join(tmpdir, 's1')),
+                ['.index.json', 'ack', 'errors'],
+            )
+            self.assertEqual(
+                listdir(join(tmpdir, 's1', 'errors')),
+                ['s1.file'],
+            )
+            self.assertEqual(
+                listdir(join(tmpdir, 's2')),
+                ['sub1', 'sub2'],
+            )
+            self.assertEqual(
+                listdir(join(tmpdir, 's2', 'sub1')),
+                ['.index.json', 'ack', 'sub1.file'],
+            )
             # no errors directory since error occured during collect, so there
             # are no file to move there
-            self.assertEqual(listdir(join(tmpdir, 's2', 'sub2')),
-                             ['.index.json', 'ack'])
+            self.assertEqual(
+                listdir(join(tmpdir, 's2', 'sub2')),
+                ['.index.json', 'ack'],
+            )
 
             with open(join(tmpdir, 's2', 'sub1', 'sub1.file')) as stream:
                 self.assertEqual(
@@ -186,14 +202,19 @@ class CollectTC(unittest.TestCase):
                         },
                     },
                     env={'TEST': 'test', 'HERE': dirname(__file__)},
-                    root_directory=tmpdir)
+                    root_directory=tmpdir,
+                )
 
             self.assertEqual(len(errors), 1)
             self.assertEqual(
                 cm.output,
-                ["ERROR:lowatt.collect:Command '{HERE}/echofile.py {DIRECTORY}/"
-                 "s1.file' is using an unknown environment variable, available "
-                 "are COLLECTOR, DIR, HERE, SOURCE, TEST"])
+                [
+                    "ERROR:lowatt.collect:Command '{HERE}/echofile.py "
+                    "{DIRECTORY}/s1.file' is using an unknown environment "
+                    "variable, available are "
+                    "COLLECTOR, DIR, HERE, SOURCE, TEST",
+                ],
+            )
 
 
 class CollectCommandsTC(unittest.TestCase):
@@ -233,8 +254,10 @@ class CollectCommandsTC(unittest.TestCase):
         )
         self.assertEqual(
             commands[1].postcollect_cmds,
-            ['echo s2.sub2 collected',
-             'echo s2.sub2 recollected'],
+            [
+                'echo s2.sub2 collected',
+                'echo s2.sub2 recollected',
+            ],
         )
 
     def test_no_postcollect(self):
@@ -263,19 +286,28 @@ class PostcollectCommandsTC(CollectCommandsTC):
                 'ERROR:lowatt.collect:No postcollect command to '
                 'handle s2/f2.csv',
                 'ERROR:lowatt.collect:No source matching s1/unknown directory',
-            ])
+            ],
+        )
 
         commands = sorted(commands, key=lambda x: x.path)
         self.assertEqual(len(commands), 3)
-        self.assertEqual(repr(commands[0]),
-                         '<PostCollectFiles s1: echo manual source>')
-        self.assertEqual([basename(file) for file in commands[0].files],
-                         ['f1.csv'])
-        self.assertEqual(repr(commands[1]),
-                         '<PostCollectFiles s2.sub1: echo s2.sub1 collected>')
-        self.assertEqual(repr(commands[2]),
-                         '<PostCollectFiles s2.sub2: echo s2.sub2 collected; '
-                         'echo s2.sub2 recollected>')
+        self.assertEqual(
+            repr(commands[0]),
+            '<PostCollectFiles s1: echo manual source>',
+        )
+        self.assertEqual(
+            [basename(file) for file in commands[0].files],
+            ['f1.csv'],
+        )
+        self.assertEqual(
+            repr(commands[1]),
+            '<PostCollectFiles s2.sub1: echo s2.sub1 collected>',
+        )
+        self.assertEqual(
+            repr(commands[2]),
+            '<PostCollectFiles s2.sub2: echo s2.sub2 collected; '
+            'echo s2.sub2 recollected>',
+        )
 
 
 class PostcollectTC(CollectCommandsTC):
@@ -291,19 +323,22 @@ class PostcollectTC(CollectCommandsTC):
                 'ERROR:lowatt.collect:No postcollect command to handle '
                 's2/f2.csv',
                 'ERROR:lowatt.collect:No source matching s1/unknown directory',
-            ])
+            ],
+        )
         self.assertEqual(len(errors), 0)
 
     def test_specified_file(self):
         root_dir = DATA_DIR
         with self.assertLogs('lowatt.collect', level='INFO') as cm:
-            postcollect(root_dir, self.sources, {},
-                        [
-                            datafile('s1', 'f1.csv'),
-                            join(THIS_DIR, 'whatever'),
-                            datafile('s2', 'f2.csv'),
-                            datafile('s3', 'unexisting'),
-                        ])
+            postcollect(
+                root_dir, self.sources, {},
+                [
+                    datafile('s1', 'f1.csv'),
+                    join(THIS_DIR, 'whatever'),
+                    datafile('s2', 'f2.csv'),
+                    datafile('s3', 'unexisting'),
+                ],
+            )
 
         self.assertEqual(
             sorted(msg.replace(THIS_DIR + '/', '') for msg in cm.output),
@@ -314,7 +349,8 @@ class PostcollectTC(CollectCommandsTC):
                 'root (data)',
                 'ERROR:lowatt.collect:Source s2 for file data/s2/f2.csv has '
                 'no postcollect command',
-            ])
+            ],
+        )
 
     def test_specified_source(self):
         root_dir = DATA_DIR
@@ -325,7 +361,8 @@ class PostcollectTC(CollectCommandsTC):
             sorted(msg.replace(THIS_DIR + '/', '') for msg in cm.output),
             [
                 'DEBUG:lowatt.collect:post collecting 1 files for source s1',
-            ])
+            ],
+        )
 
 
 class SourceDefsTC(CollectCommandsTC):
@@ -409,8 +446,10 @@ class CLITC(unittest.TestCase):
                 run()
 
         self.assertEqual(cm.exception.code, 1)
-        self.assertTrue(stream.getvalue().startswith('usage:'),
-                        stream.getvalue().splitlines()[0] + '...')
+        self.assertTrue(
+            stream.getvalue().startswith('usage:'),
+            stream.getvalue().splitlines()[0] + '...',
+        )
 
     def test_unexisting_sources_file(self):
         sys.argv = ['lowatt-collect', 'collect', 'unexisting_sources.yml']
@@ -425,7 +464,8 @@ class CLITC(unittest.TestCase):
         output = stream.getvalue()
         self.assertTrue(
             output.startswith('An error occured while reading sources file:'),
-            output.splitlines()[0] + '...')
+            output.splitlines()[0] + '...',
+        )
 
     def test_collect_specific_source_extra_args(self):
         sys.argv = ['lowatt-collect', 'collect',
